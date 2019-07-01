9d5feedd9dc42dff1afe75e8dde033f0add0dd5c
Denny Biasiolli <denny.biasiolli@gmail.com>
2019-07-01 17:18:32 +0200

    store: implementing getActivity async action

--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
+import { Activity } from '@/models/Activity';
+import { getActivity } from '@/services';
+
 Vue.use(Vuex);
+
+export interface State {
+  randomActivity?: Activity;
+}
 
 export default new Vuex.Store({
   state: {
-
-  },
+    randomActivity: undefined,
+  } as State,
   mutations: {
     initializeStore(state) {
       // Replace the state object with the stored item
@@ -15,8 +22,14 @@ export default new Vuex.Store({
           state,
           JSON.parse(localStorage.getItem('bored-app-store') || '{}')));
     },
+    updateRandomActivity(state, randomActivity: Activity) {
+      state.randomActivity = randomActivity;
+    },
   },
   actions: {
-
+    async getActivity({ state, commit }) {
+      const randomActivity = await getActivity();
+      commit('updateRandomActivity', randomActivity);
+    },
   },
 });
